794239a57a374cac02e84fb6b2bd35ec2dd974d2
Fang Jian <fangjianmail@126.com>
2018-01-23 10:39:32 +0800

    add comment

--- myroute.js
+++ myroute.js
@@ -10,9 +10,10 @@ const messageBuilder=require('./message_builder');
 router.use('/', function(req, res, next) {
     const body=req.body;
     console.log(body);
-    const query=_.get(body,"request.intent.query");
+    const query=_.get(body,"request.intent.query");//用户说的话
+    console.log(query);
     const requestType=_.get(body,"request.intent.request_type"); //Start,Intent,End
-    var message;
+    var message;//我应答的话
     if(requestType==="Start"){
         message=messageBuilder.buildResponseSimple("演出开始了",false);
     }else{
